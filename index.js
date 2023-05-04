c2355bdd11c4cdb3cc651120159906d2a5644042
adilraheel602 <adilraheel602@gmail.com>
2023-05-04 18:26:29 +0500

    new file:   .gitignore 	new file:   index.js 	new file:   index.ts 	new file:   package-lock.json 	new file:   package.json 	new file:   tsconfig.json

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const select = async () => {
+    const input = await inquirer.prompt([
+        {
+            type: "string",
+            name: "option",
+            message: "Enter 1 if you perform this again otherwise press 2:",
+        }
+    ]);
+    if (input.option == 1) {
+        await welcome();
+    }
+    else {
+        console.log("GoodBye!");
+    }
+};
+const welcome = async () => {
+    const input = await inquirer.prompt([
+        {
+            type: "string",
+            name: "sentence",
+            message: "Enter a English passage:",
+        }
+    ]);
+    const word_counter = input.sentence.trim().split(" ");
+    console.log(word_counter);
+    console.log(word_counter.length);
+    await select();
+};
+await welcome();
